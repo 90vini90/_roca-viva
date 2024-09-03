d30b6fd765c407040d09ca45a1e3ab0904a0fe48
Dev: Marcus Vinícius da Silva <mv.12ds@gmail.com>
2024-09-03 18:52:14 -0300

    Criei a parte da conta

---
+++ script.js
@@ -0,0 +1,10 @@
+// var nome = window.prompt('Qual é o seu nome?')
+var nomeInput = window.document.querySelector('input#nomedaconta')
+var emailInput = window.document.querySelector('input#emaildaconta')
+var genInput = window.document.getElementsByName('gen')
+var res = window.document.querySelector('strong#res')
+var botao = window.document.querySelector('input#botao')
+
+function clicar() {
+    res.innerHTML = 'Bem vindo ao nosso site'
+}
